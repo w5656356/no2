16f044e959ea3ae3ee9d8401593356a7bbfe6a91
xiangbingyao <358251867@qq.com>
2017-09-01 16:23:51 +0800

    最终上传

--- js/cart.js
+++ js/cart.js
@@ -16,12 +16,18 @@ require(["config"],function(){
 				})
 				$("#total_end").text(last_money)
 			}
-			
+			var lala = 0
 			$("#all_check").click(function(){
-				if(this.checked)
-				$("table .self_check").attr("checked","checked")
-				if(this.checked !== "checked")
-				$("table .self_check").removeAttr("checked")
+				if(lala === 0){
+					$("table .self_check").removeAttr("checked")
+					lala = 1
+				}
+				
+				if(lala === 1){
+					$("table .self_check").attr("checked","checked")
+					lala = 0
+				}
+				
 				add()
 			})
 			$(".self_check").click(function(){
@@ -29,6 +35,30 @@ require(["config"],function(){
 			})
 			//添加商品存数据
 				$(".decrease").click(function(){
+					var _id = $(this).parents("tr").find(".td_gold").text()
+					var _products = $.cookie("products")
+					
+					var index = isExist(_id, _products);
+					console.log(index)
+					function isExist(a,b){
+						var len = b.length
+						for(var i = 0; i < len ; i++){	
+							if(a === b[i].id){
+								return b[i]
+							}
+							
+						}
+					}
+
+					if (index.amount > 1) { // 不存在
+						// 向数组中添加元素
+						index.amount--
+						$.cookie("products", _products, {expires:10});
+					} 
+					// 将数组存回到 cookie 中
+					
+					
+					
 					var _row = $(this).parents("tr").find(".snum")
 					var _row2 = $(this).parents("tr").find(".td_price")
 					var _row3 = $(this).parents("tr").find(".td_total")
@@ -38,9 +68,31 @@ require(["config"],function(){
 							var mobi = _row2.text().slice(1)*_row.val()
 							_row3.text("￥"+mobi)
 						}	
+						
+						add()
 					})
 			
 				$(".increase").click(function(){
+					
+					var _id = $(this).parents("tr").find(".td_gold").text()
+					var _products = $.cookie("products")
+					
+					var index = isExist(_id, _products);
+					console.log(index)
+					function isExist(a,b){
+						var len = b.length
+						for(var i = 0; i < len ; i++){	
+							if(a === b[i].id){
+								return b[i]
+							}
+							
+						}
+					}
+
+						index.amount++
+						$.cookie("products", _products, {expires:10});
+					
+					
 					var _row = $(this).parents("tr").find(".snum")
 					var _row2 = $(this).parents("tr").find(".td_price")
 					var _row3 = $(this).parents("tr").find(".td_total")
@@ -49,6 +101,8 @@ require(["config"],function(){
 					
 					var mobi = _row2.text().slice(1)*_row.val()
 					_row3.text("￥"+mobi)
+					
+					add()
 				})
 				
 				$(".snum").blur(function(){
@@ -63,12 +117,51 @@ require(["config"],function(){
 					return
 					}
 					else{
+					
+					var _id = $(this).parents("tr").find(".td_gold").text()
+					var _products = $.cookie("products")
+					
+					var index = isExist(_id, _products);
+					console.log(index)
+					function isExist(a,b){
+						var len = b.length
+						for(var i = 0; i < len ; i++){	
+							if(a === b[i].id){
+								return b[i]
+							}
+							
+						}
+					}
+
+						index.amount = $(this).parents("tr").find(".snum").val()
+						$.cookie("products", _products, {expires:10});
+					
 					var mobi = _row2.text().slice(1)*_row.val()
-					_row3.text("￥"+mobi)	
+					_row3.text("￥"+mobi)
+					
+					add()
 					}
 				})
 				$(".td_del a").click(function(){
+					var _id = $(this).parents("tr").find(".td_gold").text()
+					var _products = $.cookie("products")
+					
+					var index = isExist(_id, _products);
+					console.log(index)
+					function isExist(a,b){
+						var len = b.length
+						for(var i = 0; i < len ; i++){	
+							if(a === b[i].id){
+								return i
+							}
+							
+						}
+					}
+					_products.splice(index,1)
+					$.cookie("products", _products, {expires:10});
+					
 					var _row = $(this).parents("tr").empty()
+					
 				})
 				
 				
